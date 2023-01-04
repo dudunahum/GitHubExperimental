0e4187d126c12ac11826586220f6472e03ef2c55
dudunahum <99251913+dudunahum@users.noreply.github.com>
2023-01-04 13:13:33 +0200

    Update aws_rds_cluster1.tf

--- terraform/aws/aws_rds_cluster1.tf
+++ terraform/aws/aws_rds_cluster1.tf
@@ -1,4 +1,4 @@
-resource "aws_rds_cluster" "positive1" {
+resource "aws_rds_cluster" "positive2" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.03.2"
